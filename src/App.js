4f2657dd34a306d4bf1ea20cf3f5ae6fba4eb1b3
Dimitri <61880632+dvarmstrong@users.noreply.github.com>
2022-05-05 13:35:33 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import react from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1>Hello Dojo !</h1><br/>
+        <h2>Things I need to do:</h2>
+      </div>
+    
+      <ul>
+          <li>Learn React</li>
+          <li>Climb Mt. Everest</li>
+          <li>Run a marathon</li>
+          <li>Feed the dogs</li>
+      </ul>
+    
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
